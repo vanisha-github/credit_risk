66f1d720617e2ee5a62023c15b8904404174cc3e
vanisha-github <vanishayadav3@gmail.com>
2026-04-26 23:47:43 +0530

    second

--- credit-ui/src/App.js
+++ credit-ui/src/App.js
@@ -224,11 +224,13 @@ export default function App() {
     };
 
     try {
-      const res  = await fetch("http://127.0.0.1:5000/predict", {
-        method:  "POST",
-        headers: { "Content-Type": "application/json" },
-        body:    JSON.stringify(payload),
-      });
+      const res  = await fetch("https://credit-risk-0low.onrender.com/predict", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(data)
+});
       const data = await res.json();
       setResult(data);
       setTimeout(() => {
