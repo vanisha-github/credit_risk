bd2334ee558ae4ab9f10ac5c49240357d292c69a
vanisha-github <vanishayadav3@gmail.com>
2026-04-26 23:31:31 +0530

    second

--- backend/app.py
+++ backend/app.py
@@ -10,8 +10,7 @@ import os
 
 app = Flask(__name__)
 CORS(app)
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+
 # =========================
 # LOAD MODEL
 # =========================
